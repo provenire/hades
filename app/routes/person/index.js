cfa63bbe4eca3e76fda7f7556fad543e72eac83b
Miles Zimmerman <miles@maztek.io>
2015-03-16 05:07:45 -0700

    Added names, affiliation index route

--- app/routes/person/index.js
+++ app/routes/person/index.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  titleToken: function(model) {
+    return model.get('name');
+  }
 });
--- app/routes/source/index.js
+++ app/routes/source/index.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(params) {
-    return this.store.find('source', params.source_id);
+  titleToken: function(model) {
+    return model.get('name');
   }
 });
